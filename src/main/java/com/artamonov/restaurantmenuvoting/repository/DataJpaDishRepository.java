1e4f52e479f5edaab28ad38bf6c713d3f13d1915
AndHisNameIsJohnCenaaa <vitalikfsoru@gmail.com>
2023-03-20 17:29:32 +0300

    update some repository methods are done

--- src/main/java/com/artamonov/restaurantmenuvoting/repository/DataJpaDishRepository.java
+++ src/main/java/com/artamonov/restaurantmenuvoting/repository/DataJpaDishRepository.java
@@ -4,6 +4,13 @@ import com.artamonov.restaurantmenuvoting.model.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface DataJpaDishRepository extends JpaRepository<Dish, Integer> {
+
+    List<Dish> getAllByRestaurantIdAndPublished(int restaurantId, LocalDate published);
+
+    Dish getByIdAndRestaurantId(int id, int restaurantId);
 }
